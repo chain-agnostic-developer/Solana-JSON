31a78a9231613c8d753ffa00631d19136729d7f2
Awesome <dev>
2021-12-18 23:39:59 +0530

    fix: Javascript fixes, solana/web3js upgrade

--- solana-json.js
+++ solana-json.js
@@ -1,131 +1,193 @@
 /*
 	Solana JSON module for storing and retrieving data from the Solana blockchain
 */
-const solanaWeb3 = require('@solana/web3.js');
-const fs = require('fs').promises;
-const BufferLayout = require('buffer-layout');
+const solanaWeb3 = require('@solana/web3.js')
+const fs = require('fs').promises
+const BufferLayout = require('buffer-layout')
 
 const solanaJSON = {
+  setupConnection: (network) => {
+    const connection = new solanaWeb3.Connection(network)
+    return connection
+  },
 
-	setupConnection: (network) => {
-		const connection = new solanaWeb3.Connection(network);
-		return connection;
-	},
+  /**
+   * Creates new user account
+   *
+   * @returns user account
+   */
+  createUser: async () => {
+    const user = new solanaWeb3.Account()
+    console.log(new Date(), `New solana account created: ${user.publicKey}`)
+    return user
+  },
 
-	createUser: async () => {
-		const user = new solanaWeb3.Account();
-		console.log(`New solana account created: ${user.publicKey}`);
-		return user;
-	},
+  /**
+   * Load a user from provided private key byte buffer array
+   *
+   * @param {private key buffer array} privateKeyBufferArray
+   * @returns loaded solana user account
+   */
+  loadUser: async (privateKeyBufferArray) => {
+    const user = new solanaWeb3.Account(privateKeyBufferArray)
+    console.log(new Date(), `Loaded solana account: ${user.publicKey}`)
+    return user
+  },
 
-	loadUser: async (privateKeyBufferArray) => {
-		const user = new solanaWeb3.Account(privateKeyBufferArray);
-		console.log(`Loaded solana account: ${user.publicKey}`);
-		return user;
-	},
+  /**
+   * Fund the give account by asking airdrop of 2 SOL
+   *
+   * @param {*} connection web3js connection Object
+   * @param {*} account account that needs fund
+   */
+  fundUser: async (connection, account) => {
+    console.log(new Date(), `Requesting airdrop funds... (this will take 30 seconds)`)
+    const res = await connection.requestAirdrop(account.publicKey, 4_000_000_000) // 1 SOL = 1,000,000,000 LAMPORTS
+    let lamports = 0
+    for (let breakCount = 0; breakCount < 100; breakCount += 10) {
+      await new Promise((r) => setTimeout(r, 3_000))
+      lamports = await connection.getBalance(account.publicKey)
+      console.log(
+        new Date(),
+        `Payer account ${account.publicKey.toBase58()} containing ${(lamports / 1_000_000_000).toFixed(4)}SOL`,
+      )
+      if (lamports > 1) breakCount = 100
+    }
+  },
 
-	fundUser: async (connection,account) => {
-		console.log(`Requesting airdrop funds... (this will take 30 seconds)`);
-		const res = await connection.requestAirdrop(account.publicKey, 10000000000); // 1 SOL = 1,000,000,000 LAMPORTS
-		let lamports = 0;
-		for (let breakCount = 0; breakCount < 100; breakCount+=10) {
-			await new Promise(r => setTimeout(r, 10000));
-			lamports = await connection.getBalance(account.publicKey);
-			console.log(`Payer account ${account.publicKey.toBase58()} containing ${(lamports / 1000000000).toFixed(2)}SOL`);
-			if (lamports > 1) breakCount = 100;
-		}
-	},
+  /**
+   * Get BufferLayout object with specified char len
+   *
+   * @param {*} characterLength character length
+   * @returns Buffer layout object
+   */
+  setDataStructure: (characterLength) => {
+    const structure = BufferLayout.struct([BufferLayout.blob(characterLength, 'txt')])
+    return structure
+  },
 
-	setDataStructure: (characterLength) => {
-		const structure = BufferLayout.struct([BufferLayout.blob(characterLength,'txt')]);
-		return structure;
-	},
+  /**
+   * Read compile smart contract file, create new account, load smart contract into it,
+   * create app account to store data, and transfer some lamports to pay rent/be rent free
+   * we create app account with sending a transaction stating programId is the owner of the account
+   * and space we want to acquire
+   *
+   * @param {*} connection 		web3js connection object
+   * @param {*} smartContract	solana smart contract object
+   * @param {*} payerAccount	account used for paying
+   * @returns
+   */
+  loadProgram: async (connection, smartContract, payerAccount) => {
+    // Load the program
+    console.log(new Date(), 'Loading program...')
 
-	loadProgram: async (connection,smartContract,payerAccount) => {
-		// Load the program
-		console.log('Loading program...');
-		const data = await fs.readFile(smartContract.pathToProgram);
-		const programAccount = new solanaWeb3.Account();
-		await solanaWeb3.BpfLoader.load(
-			connection,
-			payerAccount,
-			programAccount,
-			data,
-			solanaWeb3.BPF_LOADER_PROGRAM_ID,
-		);
-		const programId = programAccount.publicKey;
-		console.log('Program loaded to account', programId.toBase58());
+    console.log(new Date(), 'Reading smart contract executable')
+    const data = await fs.readFile(smartContract.pathToProgram)
 
-		// Create the app account
-		const appAccount = new solanaWeb3.Account();
-		const appPubkey = appAccount.publicKey;
-		console.log('Creating app account', appPubkey.toBase58());
-		const space = smartContract.dataLayout.span;
-		const lamports = 1000000000;
-		console.log(`Transferring ${(lamports/1000000000).toFixed(4)}SOL`);
-		const transaction = new solanaWeb3.Transaction().add(
-			solanaWeb3.SystemProgram.createAccount({
-				fromPubkey: payerAccount.publicKey,
-				newAccountPubkey: appPubkey,
-				lamports,
-				space,
-				programId,
-			}),
-		);
-		await solanaWeb3.sendAndConfirmTransaction(
-			connection,
-			transaction,
-			[payerAccount, appAccount],
-			{
-				commitment: 'singleGossip',
-				preflightCommitment: 'singleGossip',
-			},
-		);
-		return { appAccount, programId };
-	},
+    console.log(new Date(), 'Create new ProgramAccount')
+    const programAccount = new solanaWeb3.Account()
 
-	pushJSON: async (connection,app,jsonString) => {
-		if (jsonString.length > 996) throw new Error({'e':'jsonString length greater than 996 chars'});
-		const paddedMsg = jsonString.padEnd(1000);
-		const buffer = Buffer.from(paddedMsg, 'utf8');
-		const instruction = new solanaWeb3.TransactionInstruction({
-			keys: [{pubkey: app.appAccount.publicKey, isSigner: false, isWritable: true}],
-			programId: app.programId,
-			data: buffer,
-		});
-		const confirmation = await solanaWeb3.sendAndConfirmTransaction(
-			connection,
-			new solanaWeb3.Transaction().add(instruction),
-			[payerAccount],
-			{
-				commitment: 'singleGossip',
-				preflightCommitment: 'singleGossip',
-			},
-		);
-		return confirmation;
-	},
+    console.log(new Date(), 'BpfLoader loading')
+    await solanaWeb3.BpfLoader.load(connection, payerAccount, programAccount, data, solanaWeb3.BPF_LOADER_PROGRAM_ID)
 
-	pullJSON: async (connection,appPubKey) => {
-		const accountInfo = await connection.getAccountInfo(appPubKey);
-		return Buffer.from(accountInfo.data).toString().substr(4,1000).trim();
-	},
+    const programId = programAccount.publicKey
+    console.log(new Date(), 'Program loaded to account', programId.toBase58())
 
-	deploy: async () => {
-		console.log('deploying...');
-		const connection = solanaJSON.setupConnection('https://devnet.solana.com');
-		const payerAccount = await solanaJSON.createUser();
-		await solanaJSON.fundUser(connection,payerAccount);
-		const smartContract = {
-			pathToProgram: './solana-json.so',
-			dataLayout: solanaJSON.setDataStructure(1000),
-		}
-		const app = await solanaJSON.loadProgram(connection,smartContract,payerAccount);
-		console.log('app',app);
-		const confirmationTicket = await solanaJSON.pushJSON(connection,app,'{"abc":123}');
-		const testJSON = solanaJSON.pullJSON(connection,app.appAccount.publicKey);
-		console.log(`Test: ${JSON.parse(testJSON).abc}`);
-	},
+    // Create the app account
+    const appAccount = new solanaWeb3.Account()
+    const appPubkey = appAccount.publicKey
+    console.log(new Date(), 'Creating app account', appPubkey.toBase58())
+    const space = smartContract.dataLayout.span
+    const lamports = 1000000000
+    console.log(new Date(), `Transferring ${(lamports / 1000000000).toFixed(4)}SOL`)
 
+    // create a transaction
+    console.log(new Date(), 'AppAccount: Creating transaction')
+    const transaction = new solanaWeb3.Transaction().add(
+      solanaWeb3.SystemProgram.createAccount({
+        fromPubkey: payerAccount.publicKey,
+        newAccountPubkey: appPubkey,
+        lamports,
+        space,
+        programId,
+      }),
+    )
+
+    // send the transaction and await for confirmation
+    console.log(new Date(), 'AppAccount: Sending transaction and awaiting confirmation', transaction)
+    await solanaWeb3.sendAndConfirmTransaction(connection, transaction, [payerAccount, appAccount], {
+      commitment: 'singleGossip',
+      preflightCommitment: 'singleGossip',
+    })
+    console.log(new Date(), 'AppAccount: Creation Transaction execution successful')
+    return { appAccount, programId }
+  },
+
+  /**
+   * Send JSON string to smart contract
+   *
+   * @param {*} connection  web3js connection object
+   * @param {*} app         app details object
+   * @param {*} jsonString  json string to be stored
+   * @returns               confirmation id
+   */
+  pushJSON: async (connection, app, payerAccount, jsonString) => {
+    if (jsonString.length > 996) throw new Error({ e: 'jsonString length greater than 996 chars' })
+
+    // pad string
+    const paddedMsg = jsonString.padEnd(1000)
+    const buffer = Buffer.from(paddedMsg, 'utf8')
+
+    // create a instruction which is part of a transaction
+    const instruction = new solanaWeb3.TransactionInstruction({
+      keys: [{ pubkey: app.appAccount.publicKey, isSigner: false, isWritable: true }],
+      programId: app.programId,
+      data: buffer,
+    })
+
+    // send transaction and await for confirmation
+    const confirmation = await solanaWeb3.sendAndConfirmTransaction(
+      connection,
+      new solanaWeb3.Transaction().add(instruction),
+      [payerAccount],
+      {
+        commitment: 'singleGossip',
+        preflightCommitment: 'singleGossip',
+      },
+    )
+    console.log(new Date(), `Data has been pushed to ${app.programId}`)
+    return confirmation
+  },
+
+  /**
+   * Get stored json in smart contract
+   *
+   * @param {*} connection web3js connection object
+   * @param {*} appPubKey App account public key
+   * @returns
+   */
+  pullJSON: async (connection, appPubKey) => {
+    console.log(new Date(), `Loading data of account '${appPubKey}'`)
+    const accountInfo = await connection.getAccountInfo(appPubKey, 'confirmed')
+    console.log(new Date(), `AccountInfo of '${appPubKey}'`, accountInfo)
+    return Buffer.from(accountInfo.data).toString().substr(4, 1000).trim()
+  },
+
+  deploy: async () => {
+    console.log('deploying...')
+    const connection = solanaJSON.setupConnection('https://devnet.solana.com')
+    const payerAccount = await solanaJSON.createUser()
+    await solanaJSON.fundUser(connection, payerAccount)
+    const smartContract = {
+      pathToProgram: './solana-json.so',
+      dataLayout: solanaJSON.setDataStructure(1000),
+    }
+    const app = await solanaJSON.loadProgram(connection, smartContract, payerAccount)
+    console.log('app', app)
+    const confirmationTicket = await solanaJSON.pushJSON(connection, app, '{"abc":123}')
+    const testJSON = await solanaJSON.pullJSON(connection, app.appAccount.publicKey)
+    console.log(`Test: ${JSON.parse(testJSON).abc}`)
+  },
 }
 
-module.exports = solanaJSON;
+module.exports = solanaJSON
--- test.js
+++ test.js
@@ -1,17 +1,27 @@
-const solanaJSON = require('./solana-json.js');
+const solanaJSON = require('./solana-json.js')
 
-(async() => {
-		console.log('deploying...');
-		const connection = solanaJSON.setupConnection('https://devnet.solana.com');
-		const payerAccount = await solanaJSON.createUser();
-		await solanaJSON.fundUser(connection,payerAccount);
-		const smartContract = {
-			pathToProgram: './solana-json.so',
-			dataLayout: solanaJSON.setDataStructure(1000),
-		}
-		const app = await solanaJSON.loadProgram(connection,smartContract,payerAccount);
-		console.log('app',app);
-		const confirmationTicket = await solanaJSON.pushJSON(connection,app,'{"abc":123}');
-		const testJSON = solanaJSON.pullJSON(connection,app.appAccount.publicKey);
-		console.log(`Test: ${JSON.parse(testJSON).abc}`);
-})();
+;(async () => {
+  console.log(new Date(), 'deploying...')
+  // const connection = solanaJSON.setupConnection('https://devnet.solana.com');
+  const connection = solanaJSON.setupConnection('http://localhost:8899')
+  const payerAccount = await solanaJSON.createUser()
+  await solanaJSON.fundUser(connection, payerAccount)
+  const smartContract = {
+    pathToProgram: './solana-json.so',
+    dataLayout: solanaJSON.setDataStructure(1000),
+  }
+  const app = await solanaJSON.loadProgram(connection, smartContract, payerAccount)
+  console.log('app', app)
+
+  console.log(new Date(), 'Pushing json to smart contracts')
+  const confirmationTicket = await solanaJSON.pushJSON(connection, app, payerAccount, '{"abc":123}')
+
+  console.log(new Date(), 'Confirmation ticket details', confirmationTicket)
+
+  console.log(new Date(), 'Pulling json data from smart contract')
+  const testJSON = await solanaJSON.pullJSON(connection, app.appAccount.publicKey)
+
+  console.log(new Date(), 'Data from solana program', testJSON)
+
+  console.log(new Date(), `Test: ${JSON.parse(testJSON).abc}`)
+})()
